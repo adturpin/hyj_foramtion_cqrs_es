90651957881fe985b02768d42889015e4de98ed9
aitrigan <adrien.turpin@hotmail.fr>
2022-10-14 18:02:03 +0200

    finish

---
+++ src/elvi.formation.cqrses.test/CommandHandler/EstablishOrganisationsCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace elvi.formation.cqrses.test.CommandHandler
+{
+    
+    public class EstablishOrganisationsCommandHandler : ICommandHandler<EstablishOrganisationsCommand>
+    {
+        private readonly IEventStore _eventStore;
+        private readonly PubSub _pubSub;
+
+        public EstablishOrganisationsCommandHandler(IEventStore eventStore, PubSub pubSub)
+        {
+            _eventStore = eventStore;
+            _pubSub = pubSub;
+        }
+        
+        public void Handler(int folderId, EstablishOrganisationsCommand command)
+        {
+            var events = _eventStore.GetEvents(folderId);
+            var folder = new ValidationFolder(events);
+            var processEvents = folder.ProcessCommand(command);
+            _pubSub.Publish(folderId, processEvents);
+        }
+    }
+
+    public interface ICommandHandler<T> where T : DomainCommand
+    {
+        void Handler(int folderId,T command);
+    }
+}
---
+++ src/elvi.formation.cqrses.test/CommandHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using elvi.formation.cqrses.test.CommandHandler;
+using NUnit.Framework;
+
+namespace elvi.formation.cqrses.test
+{
+    public class CommandHandlerTests
+    {
+
+
+        [Test]
+        public void ShouldReadModelUpdateByCommandHandler()
+        {
+
+            var (dispatcher, commandHandler) = CreateCommandHandler();
+            int folderId = 14;
+            EstablishOrganisationsCommand command = new EstablishOrganisationsCommand(new List<string>() {"test"});
+            
+            commandHandler.Handler(folderId, command);
+
+            Assert.IsTrue(dispatcher.Called);
+        }
+
+        private static (OrgannisationValidationListedEventHandlerDispatcher, EstablishOrganisationsCommandHandler) CreateCommandHandler()
+        {
+            IEventStore eventStore = new InMemoryEventStore();
+
+            var dispatcher = new OrgannisationValidationListedEventHandlerDispatcher();
+
+            Dictionary<Type, List<object>> dispatchers = new Dictionary<Type, List<object>>()
+            {
+                {typeof(OrgannisationValidationListed), new List<object>() {dispatcher}}
+            };
+
+            PubSub pubSub = new PubSub(eventStore, dispatchers);
+
+            var commandHandler = new EstablishOrganisationsCommandHandler(eventStore, pubSub);
+            return (dispatcher, commandHandler);
+        }
+    }
+}
